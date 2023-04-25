58b8017783d18258ec3d8a9396ec3b7c16908d26
Vinícius Patrício <vpms@icomp.ufam.edu.br>
2023-04-25 19:56:03 -0400

    Criando um app

--- projeto1/urls.py
+++ projeto1/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse('Home')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view),  # home
+    path('sobre/', sobre),  # sobre
+    path('contato/', contato)  # contato
 ]
